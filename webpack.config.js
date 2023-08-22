be8b07abb6338ac4255b8061801af739c7129e86
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-08-22 01:00:09 -0700

    Set up tools for project. Install webpack, eslint and prettier, and babel

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        },
+        {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+            presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+            ]
+            }
+        }
+        }
+    ]
+  }
+};
