e9ff4d0c6f78b9b08920db81cd8ace66f7998d35
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-08-23 21:19:07 -0700

    feat: Add temperature unit toggle functionality
    Added a button to the weather display that allows users to toggle between Fahrenheit and Celsius temperature units.

--- src/display.js
+++ src/display.js
@@ -1,5 +1,10 @@
-const currentUnit = 'c';
+let currentUnit = 'c';
 
+function getCurrentUnit(){
+    return currentUnit;
+}
+
+// Fetch weather data using weather API
 async function getWeather(unit, location) {
     const data = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=bdbddb896e8c4f5e89704849232208&q=${location}}&unit=${unit}`,
@@ -8,7 +13,9 @@ async function getWeather(unit, location) {
     return processed;
 }
 
-async function displayData(keyWord) {
+// Display weather information on wepage
+async function displayData(keyWord, unit) {
+    currentUnit = unit;
     // Clear previous location
     const cont = document.querySelector('.bottomHalf');
     while(cont.firstChild) cont.removeChild(cont.firstChild);
@@ -16,27 +23,62 @@ async function displayData(keyWord) {
     try {
         const data = await getWeather(currentUnit, keyWord);
 
+        // Select desired data and create corresponding element
         const location = `${data.location.name}, ${data.location.region}`;
         const time = data.location.localtime;
-
         const condition = data.current.condition.text;
-
         const temperatureUnit = currentUnit === 'f' ? 'Fahrenheit' : 'Celsius';
-
         const currentTemp = data.current[`temp_${currentUnit}`];
         const feelsLike = data.current[`feelslike_${currentUnit}`];
         const {humidity} = data.current;
-
+        
         const weatherData = document.createElement('div');
-        weatherData.innerHTML = `
-            <h1>Location: ${location}</h1>
-            <h1>Last Updated: ${time}</h1>
-            <h1>Condition: ${condition}</h1>
-            <h1>Temperature Unit: ${temperatureUnit}</h1>
-            <h1>Current Temperature: ${currentTemp}</h1>
-            <h1>Feels Like: ${feelsLike}</h1>
-            <h1>Humidity: ${humidity}%</h1>
-        `;
+        
+        const locationHeading = document.createElement('h1');
+        locationHeading.textContent = `Location: ${location}`;
+        weatherData.appendChild(locationHeading);
+        
+        const timeHeading = document.createElement('h1');
+        timeHeading.textContent = `Last Updated: ${time}`;
+        weatherData.appendChild(timeHeading);
+        
+        const conditionHeading = document.createElement('h1');
+        conditionHeading.textContent = `Condition: ${condition}`;
+        weatherData.appendChild(conditionHeading);
+        
+        const tempUnitDiv = document.createElement('div');
+        const tempUnitHeading = document.createElement('h1');
+        tempUnitHeading.textContent = `Temperature Unit: ${temperatureUnit}`;
+        tempUnitDiv.appendChild(tempUnitHeading);
+        
+        const toggleButton = document.createElement('button');
+        toggleButton.textContent = 'Toggle Unit';
+        toggleButton.addEventListener('click', () => {
+            displayData(keyWord, currentUnit === 'f' ? 'c' : 'f');
+        });
+        toggleButton.style.height='fit-content';
+        toggleButton.style.fontSize = '1.5em';
+
+        tempUnitDiv.appendChild(toggleButton);
+        tempUnitDiv.style.display = 'flex';      
+        tempUnitDiv.style.gap = '4px';  
+        tempUnitDiv.style.alignItems = 'center';
+        tempUnitDiv.style.justifyContent='space-between';
+        weatherData.appendChild(tempUnitDiv);
+        
+        const currentTempHeading = document.createElement('h1');
+        currentTempHeading.textContent = `Current Temperature: ${currentTemp}`;
+        weatherData.appendChild(currentTempHeading);
+        
+        const feelsLikeHeading = document.createElement('h1');
+        feelsLikeHeading.textContent = `Feels Like: ${feelsLike}`;
+        weatherData.appendChild(feelsLikeHeading);
+        
+        const humidityHeading = document.createElement('h1');
+        humidityHeading.textContent = `Humidity: ${humidity}%`;
+        weatherData.appendChild(humidityHeading);
+        
+        weatherData.classList.add("data");
         cont.appendChild(weatherData);
     } catch (error) {
         const errorMsg = document.createElement('div');
@@ -45,5 +87,5 @@ async function displayData(keyWord) {
     }
 }
 
-export default displayData;
+export {displayData, getCurrentUnit};
 
