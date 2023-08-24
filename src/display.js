dcedf20758f26ef62000dd0c5ebbda90272159c8
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-08-24 11:15:20 -0700

    fix: Include CORS mode for API request

--- src/display.js
+++ src/display.js
@@ -8,6 +8,9 @@ function getCurrentUnit() {
 async function getWeather(unit, location) {
   const data = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=bdbddb896e8c4f5e89704849232208&q=${location}}&unit=${unit}`,
+    {
+      mode: 'cors',
+    },
   );
   const processed = await data.json();
   return processed;
